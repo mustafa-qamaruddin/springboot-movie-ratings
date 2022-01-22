5d36be1fb071f4bae433f127ab317a29be16814d
mustafa <m.qamaruddin.89@gmail.com>
2022-01-22 22:13:20 +0100

    Add unit test

--- src/main/java/com/iomoto/demo/services/VehicleService.java
+++ src/main/java/com/iomoto/demo/services/VehicleService.java
@@ -5,11 +5,8 @@ import com.iomoto.demo.models.VehicleModel;
 import com.iomoto.demo.repositories.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class VehicleService {
