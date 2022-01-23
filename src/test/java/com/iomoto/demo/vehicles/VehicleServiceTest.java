1f2a02ba698ec3cb9982b49d1f31be6a9330e79f
mustafa <m.qamaruddin.89@gmail.com>
2022-01-23 12:33:48 +0100

    Fixed test containers

--- src/test/java/com/iomoto/demo/vehicles/VehicleServiceTest.java
+++ src/test/java/com/iomoto/demo/vehicles/VehicleServiceTest.java
@@ -6,39 +6,65 @@ import com.iomoto.demo.services.VehicleService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.HashMap;
 
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@Testcontainers
+
+@ExtendWith(SpringExtension.class)
 @DataMongoTest(
         includeFilters = @ComponentScan.Filter(
                 type = FilterType.ASSIGNABLE_TYPE,
                 classes = {
-                        MongoTemplate.class,
-                        VehicleRepository.class,
                         VehicleService.class
                 }
         )
 )
-@AutoConfigureDataMongo
+@Testcontainers
+@ContextConfiguration(initializers = VehicleServiceTest.Initializer.class)
 public class VehicleServiceTest {
 
     @Autowired
     VehicleService vehicleService;
 
     @Container
-    final MongoDBContainer mongoDBContainer = new MongoDBContainer(
+    public static final MongoDBContainer mongoDBContainer = new MongoDBContainer(
             DockerImageName.parse("mongo:4.0.10")
     );
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+                    configurableApplicationContext,
+                    format(
+                            "spring.data.mongodb.uri=mongodb://%s:%s/testDB",
+                            mongoDBContainer.getContainerIpAddress(),
+                            mongoDBContainer.getMappedPort(27017)
+                    )
+            );
+        }
+    }
 
     @BeforeEach
     void setUp() {
